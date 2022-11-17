106c1ad6147826645383ed04ddb7015fb096647e
Luth-Badila <FrancescoLutfi10@gmail.com>
2022-11-17 13:10:27 +0700

    made mazas blog with resposive navbar

---
+++ pages/Menu.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Link from "next/link";
+
+const Menu = () => {
+  return <div className="flex justify-between items-center">Menu</div>;
+};
+
+export default Menu;
